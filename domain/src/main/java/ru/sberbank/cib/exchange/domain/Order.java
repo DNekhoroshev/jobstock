df4061da5f8376ee760d409289b24c041b66c0c1
DimaKroshkin <dima.kroshkin@gmail.com>
2018-11-01 14:51:18 +0400

    Order class

--- domain/src/main/java/ru/sberbank/cib/exchange/domain/Order.java
+++ domain/src/main/java/ru/sberbank/cib/exchange/domain/Order.java
@@ -4,14 +4,42 @@ public class Order {
 	private int id;
 	private String description;
 	
+//	private int skillId;
+//	private int level;
+//	private int weight;	
+//	private Skill skill;
+//	private SkillLevel level;
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+ 
 	public int getId() {
 		return id;
 	}
 	
-	public Order(int id, String description) {
-		this.id = id;
+	public void setDescription(String description) {
 		this.description = description;
-			
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	@Override
+	public int hashCode() {
+		return id;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (obj.getClass() != Order.class) {
+			return false;
+		}
+		return this.id == ((Order) obj).id;
 	}
 	
 	@Override
