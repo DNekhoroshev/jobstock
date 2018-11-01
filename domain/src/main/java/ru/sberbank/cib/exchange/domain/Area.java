a99bf32ded0e87b1de039c6109b9186ed05d95db
DimaKroshkin <dima.kroshkin@gmail.com>
2018-11-01 14:17:47 +0400

    Area class

--- domain/src/main/java/ru/sberbank/cib/exchange/domain/Area.java
+++ domain/src/main/java/ru/sberbank/cib/exchange/domain/Area.java
@@ -4,6 +4,38 @@ public class Area {
 	public int id;
 	public String name;
 	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public int hashCode() {
+		return id;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (obj.getClass() != Area.class) {
+			return false;
+		}
+		return this.id == ((Area) obj).id;
+	}
+	
 	@Override
 	public String toString() {
 		return "Area " + name + "(" + id + ")"; 
