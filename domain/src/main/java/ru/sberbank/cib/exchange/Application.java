8ad870c19bb4a0c4bbcbf198737de292c9e53b1a
DimaKroshkin <dima.kroshkin@gmail.com>
2018-11-01 16:30:08 +0400

    add database dependencies(draft)

--- domain/src/main/java/ru/sberbank/cib/exchange/Application.java
+++ domain/src/main/java/ru/sberbank/cib/exchange/Application.java
@@ -1,8 +1,17 @@
 package ru.sberbank.cib.exchange;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Application {
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 	
 	public static void main(String[] args) {
+		logger.info("Starting");
+		ApplicationContext context = new ClassPathXmlApplicationContext("/application.xml");
+		logger.info("Started");
 		
 	}
 	
